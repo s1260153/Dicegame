f0c997fd130998068fdd4317fb868efe28a5be88
s1260153 <s1260153@u-aizu.ac.jp>
2019-07-06 02:26:04 +0900

    fourth commit by master

--- dicegame.c
+++ dicegame.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 int main(){
-  int a,b;
+  int a,b,c;
   char name[50];
 
   printf("What is your name?\n");
@@ -14,16 +14,18 @@ int main(){
   srand(time(NULL));
   a = rand() % 6 + 1;
   b = rand() % 6 + 1;
+  c = rand() % 6 + 1;
 
 
   printf("Rooling the dice... \n");
 
   printf("Die 1: %d\n",a);
   printf("Die 2: %d\n",b);
+  printf("Die 3: %d\n",c);
 
-  printf("Total value: %d\n", a+b);
+  printf("Total value: %d\n", a+b+c);
 
-  if(a+b > 7){
+  if(a+b+c > 15){
     printf("%s won!\n",name);
   }
   else{
